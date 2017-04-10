6a430bbe421f93ec5bdcbeb5ad5faa9e061e3ca9
Courtney Beames <cbeames@uvic.ca>
2017-04-09 17:58:00 -0700

    All notes are in and camera moves along while playing

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+	public float increment = 0.01f;
+	// Use this for initialization
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 curPos = transform.localPosition;
+		curPos[0] += increment;
+		transform.localPosition = curPos;
+	}
+}
